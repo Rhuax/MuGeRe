1f991b4e62b240a86af3cb8417832c2eec167c9d
crow <gelidcrow@gmail.com>
2017-11-01 19:55:37 +0100

    Improved script. It now jumps when a spectrograms has been already created.

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # MuGeRe
 MUsic GEnre REcognition
+
+This is an attempt to perform music genre recognition directly from audio with deep convolutional neural networks.
+The basic process goes from generating spectrograms using "SoX" into png files. PNG files are chunked and each chunk represents a training instance for the learning algorithm.
+Every chunk is then fed to the CNN which extracts salient features to be passed to fully connected layers in order to perform categorical prediction.
+![alt tag](https://raw.githubusercontent.com/rhuax/rix/master/anim.gif)
+![alt tag](https://raw.githubusercontent.com/rhuax/rix/master/anim.gif)
+![alt tag](https://raw.githubusercontent.com/rhuax/rix/master/anim.gif)
+![alt tag](https://raw.githubusercontent.com/rhuax/rix/master/anim.gif)
+![alt tag](https://raw.githubusercontent.com/rhuax/rix/master/anim.gif)
+
+#Requirements
+
+* Tensorflow
+* Keras
+* Pillow
+* Pydub
+* sox
+* ffmpeg
--- generate_spectrogram_linux.py
+++ generate_spectrogram_linux.py
@@ -5,12 +5,11 @@ import os
 from subprocess import run, PIPE
 from PIL import Image
 from pydub import AudioSegment
+from pydub.exceptions import CouldntDecodeError
 dataset='fma_small/'
 final_dataset='fma_final/'
 
 def stereo2mono(a):
-
-
     c='sox '+a+' temp.mp3 remix 1,2'
     run(c, shell=True, stdin=PIPE, stdout=PIPE)
 def audio2spec():
@@ -23,7 +22,6 @@ def split_chunks(base_name):
         box=(j,0,j+160,150)
         crop=i.crop(box)
         crop.save(base_name+'_'+str(j//160)+'.png')
-
 
 with open('dataset.csv',mode='r') as f:
     reader=csv.reader(f)
@@ -38,23 +36,35 @@ for f in glob.glob(dataset+'*/*.mp3'):
     genre=dict[os.path.basename(f)[0:-4]]
     if not os.path.exists(final_dataset+genre):
         os.mkdir(final_dataset+genre)
+    name=os.path.basename(f)[0:-4]
+    name+='_0.png'
+    found=False
+    for d,s,fii in os.walk(final_dataset):
+        if name in fii:
+            found=True
+            print("Salto")
+            break
+    if found==False:
+        try:
+            audio = AudioSegment.from_mp3(os.path.abspath(f))
+            if audio.channels==2:
+                stereo2mono(os.path.abspath(f))
+            else:
+                copyfile(os.path.abspath(f),'temp.mp3')
 
-    audio = AudioSegment.from_mp3(os.path.abspath(f))
-    if audio.channels==2:
-        stereo2mono(os.path.abspath(f))
-    else:
-        copyfile(os.path.abspath(f),'temp.mp3')
+                mono+=1
 
-        mono+=1
+            audio2spec()
+            split_chunks(final_dataset+genre+'/'+os.path.basename(f)[0:-4])
 
-    audio2spec()
-    split_chunks(final_dataset+genre+'/'+os.path.basename(f)[0:-4])
-    i+=1
-    a=(i/40000)*100
-    a=int(a)
-    if a!=prev:
-        print(str(a)+'%')
-        prev=a
+        except CouldntDecodeError:
+            print("Un mp3 fa cagare al cazzo")
+    i += 1
+    a = (i / 8000) * 100
+    a = int(a)
+    if a != prev:
+        print(str(a) + '%')
+        prev = a
 
 
 print("Trovati "+str(mono)+" file mono -.-'")
