803828109bba2913546225dc40c90ac470d8a13d
Stefano Romanazzi <stefan.romanazzi@gmail.com>
2017-11-01 16:06:45 +0100

    Spectrograms generation
    - gitignore with .idea
    - conversion of audio tracks from stereo to mono
    - spectrogram generation with resolution 800x150
    - save to path

--- generate_ground_truth.py
+++ generate_ground_truth.py
@@ -8,77 +8,78 @@ It is used as ground truth in the learning process.
 import csv
 import glob
 
+
 def getTopgenre(id_subgenre):
     reader = csv.reader(open('genres.csv'), delimiter=",")
     reader2 = csv.reader(open('genres.csv'), delimiter=",")
 
     for line in reader:
-        if line[0]==id_subgenre:
+        if line[0] == id_subgenre:
             for l in reader2:
-                if l[0]==line[-1]:
+                if l[0] == line[-1]:
                     return l[-2]
 
-
-
-
-
 def extract_trackid_topgenres():
-    reader=csv.reader(open('tracks.csv'),delimiter=",")
-    #13 genre top
-    i=0
+    reader = csv.reader(open('tracks.csv'), delimiter=",")
+    # 13 genre top
+    i = 0
     for line in reader:
-        i+=1
-        if i==3:
+        i += 1
+        if i == 3:
             break
-    file=open('training_set.csv','w')
-    dict={'Hip-Hop':0,'Pop':0,'Rock':0,'Experimental':0,'Folk':0,'Jazz':0,'Electronic':0,'Spoken':0,
-          'International':0,'Soul-RnB':0,'Blues':0,'Country':0,'Classical':0,'Old-Time / Historic':0,
-          'Instrumental':0,'Easy Listening':0}
-    #writer.writerow(['track_id','genre'])
+    file = open('training_set.csv', 'w')
+    dict = {'Hip-Hop': 0, 'Pop': 0, 'Rock': 0, 'Experimental': 0, 'Folk': 0, 'Jazz': 0, 'Electronic': 0, 'Spoken': 0,
+            'International': 0, 'Soul-RnB': 0, 'Blues': 0, 'Country': 0, 'Classical': 0, 'Old-Time / Historic': 0,
+            'Instrumental': 0, 'Easy Listening': 0}
+    # writer.writerow(['track_id','genre'])
     for line in reader:
-        genre=line[-13]
-        track_id=line[0]
-        if(genre==''):
-            #Retrieve its top genre
-            sub=line[-12]
-            sub=sub[1:-1]
-            sub=sub.split(',')
-            genre=getTopgenre(sub[0])
-        if genre is not None and genre!='':
-            file.write(track_id+','+genre+'\n')
-            dict[genre]=dict[genre]+1
+        genre = line[-13]
+        track_id = line[0]
+        if (genre == ''):
+            # Retrieve its top genre
+            sub = line[-12]
+            sub = sub[1:-1]
+            sub = sub.split(',')
+            genre = getTopgenre(sub[0])
+        if genre is not None and genre != '':
+            file.write(track_id + ',' + genre + '\n')
+            dict[genre] = dict[genre] + 1
 
     for k in dict.keys():
-
-        print(k,end='')
-        print(' ',end='')
+        print(k, end='')
+        print(' ', end='')
         print(dict[k])
 
     print(dict)
     file.close()
 
+
 """Generate the effective training_set since not all metadata tracks are in the
 small version of fma dataset"""
+
 def generate_true_dataset():
     reader = csv.reader(open('training_set.csv'), delimiter=",")
-    file=open('dataset.csv','w')
+    file = open('dataset.csv', 'w')
     for line in reader:
-        id=line[0].zfill(6) #Pad with zeros since mp3 files are padded with zeros -_-"
+        id = line[0].zfill(6)  # Pad with zeros since mp3 files are padded with zeros -_-"
 
-        #Check if a file named id.mp3 is present in one of all sub-folders
-        for f in glob.glob('fma_small/*/'+id+'.mp3',recursive=True):
-            #I'm inside the loop so there's a file
-            file.write(id+','+line[1]+'\n')
+        # Check if a file named id.mp3 is present in one of all sub-folders
+        for f in glob.glob('fma_small/*/' + id + '.mp3', recursive=True):
+            # I'm inside the loop so there's a file
+            file.write(id + ',' + line[1] + '\n')
 
     file.close()
+
+
 def count_genres(file):
     reader = csv.reader(open(file), delimiter=",")
     dict = {'Hip-Hop': 0, 'Pop': 0, 'Rock': 0, 'Experimental': 0, 'Folk': 0, 'Jazz': 0, 'Electronic': 0, 'Spoken': 0,
             'International': 0, 'Soul-RnB': 0, 'Blues': 0, 'Country': 0, 'Classical': 0, 'Old-Time / Historic': 0,
             'Instrumental': 0, 'Easy Listening': 0}
     for line in reader:
-        dict[line[1]]=dict[line[1]]+1
+        dict[line[1]] = dict[line[1]] + 1
     for k in dict.keys():
-        print(k+' '+str(dict[k]))
+        print(k + ' ' + str(dict[k]))
+
 
 count_genres('dataset.csv')
---
+++ generate_spectograms.py
@@ -0,0 +1,41 @@
+import os
+import csv
+from subprocess import run, PIPE
+
+rootdir = '\\fma_small'
+dataset_mono_root = '\\fma_small_final'
+
+def set_to_mono(path_file, tmp_name):
+    command = "C:\\Users\\Stefano\\Desktop\\prova\\sox-14-4-2\\sox.exe {} {} remix 1,2".format(path_file, tmp_name)
+    run(command, shell=True, stdin=PIPE, stdout=PIPE)
+
+# converts the audio to spectrogram
+def audio_to_spect(input_file, output_file):
+    command = "C:\\Users\\Stefano\\Desktop\\prova\\sox-14-4-2\\sox.exe {} -n spectrogram -r -x 800 -y 150 -o {}".format(input_file, output_file)
+    run(command, shell=True, stdin=PIPE, stdout=PIPE)
+
+# helper function to delete files no longer needed
+def delete_file(file_path):
+    os.remove(file_path)
+
+def get_genre_from_track(track_name):
+    reader = csv.reader(open('dataset.csv'), delimiter=",")
+    for line in reader:
+        if line[0] == track_name:
+            return line[1]
+
+
+for subdir, dirs, files in os.walk(rootdir):
+    for file in files:
+        print(os.path.join(subdir, file))
+        path_file = subdir + "\\" + file
+        mono_name = dataset_mono_root + "\\" + get_genre_from_track(file[0:-4]) + "\\" + file[0:-4] + "_mono.mp3"
+        new_path = dataset_mono_root + "\\" + get_genre_from_track(file[0:-4]) + "\\"
+        if not os.path.exists(new_path):
+            os.makedirs(new_path)
+
+        set_to_mono(path_file, mono_name)
+        spec_file = new_path + file[0:-4] + ".png"
+        audio_to_spect(mono_name, spec_file)
+        delete_file(mono_name)
+
