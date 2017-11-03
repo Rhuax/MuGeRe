491a3501ee7994664a98fe984cb22cb6f864866a
Francesco Suriano <fra.suri94@gmail.com>
2017-11-03 17:39:38 +0100

    trying other architectures

--- dataset_load.py
+++ dataset_load.py
@@ -13,41 +13,42 @@ import os
 
 
 batchSize = 32
-epochs = 50
+epochs = 30
 num_classes = 8
-model_name = 'Genre Recognition Model'
 
 trainsetDir = 'fma_train/'
 testsetDir = 'fma_test/'
 
 # Data generators
-train_datagen = ImageDataGenerator()
-test_datagen = ImageDataGenerator()
+train_datagen = ImageDataGenerator(rescale=1/255)
+test_datagen = ImageDataGenerator(rescale=1/255)
 
 train_generator = train_datagen.flow_from_directory(directory=trainsetDir, batch_size=batchSize, target_size=(160,150), shuffle=True)
 test_generator = test_datagen.flow_from_directory(directory=testsetDir, batch_size=batchSize, target_size=(160,150))
 
 model = Sequential()
 
-model.add(Conv2D(input_shape=(160, 150, 3), filters=32, kernel_size=(2,2), strides=2, activation="elu"))
+model.add(Conv2D(input_shape=(160, 150, 3), filters=32, kernel_size=(3,3), strides=2, activation="elu", kernel_regularizer='l2'))
 model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
 
-model.add(Conv2D(filters=64, kernel_size=(2,2), strides=2, activation="elu"))
+model.add(Conv2D(filters=64, kernel_size=(2,2), strides=2, activation="elu", kernel_regularizer='l2'))
 model.add(MaxPooling2D(pool_size=(2, 2), strides=(2,2)))
 
-model.add(Conv2D(filters=128, kernel_size=(2,2), strides=2, activation="elu"))
+model.add(Conv2D(filters=128, kernel_size=(2,2), strides=2, activation="elu", kernel_regularizer='l2'))
 model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
 
 model.add(Flatten())
-model.add(Dense(256, activation='elu'))
-#model.add(Dropout(0.25))
-model.add(Dense(512, activation='elu'))
+model.add(Dense(512, activation='elu', kernel_regularizer='l2'))
+model.add(Dropout(0.7))
+model.add(Dense(256, activation='elu', kernel_regularizer='l2'))
 model.add(Dropout(0.5))
-model.add(Dense(8, activation='softmax'))
+model.add(Dense(128, activation='elu', kernel_regularizer='l2'))
+model.add(Dropout(0.3))
+model.add(Dense(num_classes, activation='softmax', kernel_regularizer='l2'))
 
 #Compile model
 model.compile(loss='categorical_crossentropy',
-              optimizer='rmsprop',
+              optimizer='adam',
               metrics=['accuracy'])
 
 #Summary
@@ -56,7 +57,7 @@ model.summary()
 #Training
 model.fit_generator(
         train_generator,
-        epochs=50,
+        epochs=epochs,
         validation_data=test_generator,
         validation_steps=8000//batchSize,
         steps_per_epoch=31985//batchSize)
