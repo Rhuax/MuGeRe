2a2cb02f630db2faa2e71fbfe806e0ba8abc7c67
Francesco Suriano <fra.suri94@gmail.com>
2017-12-25 18:10:51 +0100

    Added folder with architectures and weights

--- training_2.py
+++ training_2.py
@@ -1,19 +1,10 @@
+
 from __future__ import print_function
-
-import keras
-
-
-
+import h5py
 from keras.preprocessing.image import ImageDataGenerator
-
-
-
 from keras.models import Sequential
-
-from keras.layers import Dense, Dropout, Activation, Flatten, ZeroPadding2D, LeakyReLU
-
+from keras.layers import Dense, Dropout, Activation, Flatten, ZeroPadding2D, LeakyReLU, Reshape
 from keras.layers import Conv2D, MaxPooling2D, AveragePooling2D
-
 from keras.optimizers import Adadelta
 
 
@@ -57,56 +48,47 @@ test_datagen = ImageDataGenerator(rescale=1./255)
 train_generator = train_datagen.flow_from_directory(directory=trainsetDir, batch_size=batchSize, target_size=(160,150), shuffle=True)
 test_generator = test_datagen.flow_from_directory(directory=testsetDir, batch_size=batchSize, target_size=(160,150))
 
-
+#kernel_regularizer='l2', kernel_initializer='glorot_normal'
 '''
 model = Sequential()
 
-model.add(Conv2D(input_shape=(160, 150, 3), filters=16, kernel_size=(3,3), strides=(2,2), padding='same', activation="elu", kernel_regularizer='l2', kernel_initializer='glorot_normal'))
-model.add(Conv2D(filters=32, kernel_size=(3,3), strides=(2,2), padding='same',activation="elu", kernel_regularizer='l2', kernel_initializer='glorot_normal'))
+model.add(Conv2D(input_shape=(160, 150, 3), filters=32, kernel_size=(2,2), strides=(2,2), activation="elu", kernel_initializer='glorot_normal'))
+model.add(Dropout(0.5))
+model.add(Conv2D(filters=64, kernel_size=(2,2), strides=(2,2),activation="elu", kernel_initializer='glorot_normal'))
 
 model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
 
-model.add(Conv2D(filters=64, kernel_size=(3,3), strides=(2,2), padding='same',activation="elu", kernel_regularizer='l2', kernel_initializer='glorot_normal'))
-model.add(Conv2D(filters=128, kernel_size=(3,3), strides=(2,2), padding='same',activation="elu", kernel_regularizer='l2', kernel_initializer='glorot_normal'))
+model.add(Conv2D(filters=128, kernel_size=(2,2), strides=(2,2), activation="elu", kernel_initializer='glorot_normal'))
+model.add(Dropout(0.5))
+model.add(Conv2D(filters=256, kernel_size=(2,2), strides=(2,2), activation="elu", kernel_initializer='glorot_normal'))
 
 model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
-
 model.add(Flatten())
-model.add(Dense(256, activation='elu', kernel_regularizer='l2'))
-#model.add(Dropout(0.5))
 
-model.add(Dense(128, activation='elu', kernel_regularizer='l2'))
-#model.add(Dropout(0.5))
+model.add(Dense(512, activation='elu', kernel_initializer='glorot_normal'))
+model.add(Dropout(0.5))
 
-model.add(Dense(64, activation='elu', kernel_regularizer='l2'))
+model.add(Dense(128, activation='elu', kernel_initializer='glorot_normal'))
+model.add(Dropout(0.5))
+
 model.add(Dense(num_classes, activation='softmax'))
 '''
 
-#activation = 'elu'
-activation = LeakyReLU()
-kernel_initializer = 'glorot_normal'
-kernel_regularizer = 'l2'
-kernel_size = (3,3)
-strides = (2,2)
-optimizer = Adadelta(lr=1.0, rho=0.95, epsilon=1e-08, decay=0.0)
 
 model = Sequential()
 
-model.add(Conv2D(input_shape=(160, 150, 3), filters=32, kernel_size=kernel_size, strides=strides, activation=activation, kernel_regularizer=kernel_regularizer, kernel_initializer=kernel_initializer))
-model.add(Dropout(0.5))
-model.add(Conv2D(filters=64, kernel_size=kernel_size, strides=strides, activation=activation, kernel_regularizer=kernel_regularizer, kernel_initializer=kernel_initializer))
-model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
-model.add(Conv2D(filters=128, kernel_size=kernel_size, strides=strides,activation=activation, kernel_regularizer=kernel_regularizer, kernel_initializer=kernel_initializer))
+model.add(Conv2D(input_shape=(160, 150, 3), filters=32, kernel_size=(5,5), strides=(5,5), activation="elu", kernel_initializer='glorot_normal'))
+model.add(Conv2D(filters=64, kernel_size=(3,3), strides=(3,3),activation="elu", kernel_initializer='glorot_normal'))
+model.add(Conv2D(filters=128, kernel_size=(2,2), strides=(2,2),activation="elu", kernel_initializer='glorot_normal'))
 model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
 model.add(Flatten())
-model.add(Dropout(0.5))
-model.add(Dense(256, activation=activation, kernel_regularizer=kernel_regularizer, kernel_initializer=kernel_initializer))
-model.add(Dropout(0.5))
-model.add(Dense(128, activation=activation, kernel_regularizer=kernel_regularizer, kernel_initializer=kernel_initializer))
-model.add(Dropout(0.5))
+#model.add(Dense(512, activation='elu', kernel_initializer='glorot_normal'))
 model.add(Dense(num_classes, activation='softmax'))
+
 
 #Compile model
+
+optimizer = 'adam'
 
 model.compile(loss='categorical_crossentropy',
 
@@ -123,7 +105,7 @@ model.summary()
 
 
 #Training
-
+#model.load_weights('MuGeRe Weights_T4.h5', by_name=True)
 model.fit_generator(
 
         train_generator,
@@ -135,3 +117,12 @@ model.fit_generator(
         validation_steps=25001//batchSize,
 
         steps_per_epoch=99924//batchSize)
+
+
+# serialize model to YAML
+model_yaml = model.to_yaml()
+with open("MuGeRe Architecture.yaml", "w") as yaml_file:
+    yaml_file.write(model_yaml)
+# serialize weights to HDF5
+model.save_weights("MuGeRe Weights.h5")
+print("Saved model to disk")
