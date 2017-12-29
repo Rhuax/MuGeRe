5b6ce34397daa2944298f8ca7dec8eb1a6c5c4f8
Rhuax <gelidcrow@gmail.com>
2017-12-29 19:44:52 +0100

    architecture test

--- README.md
+++ README.md
@@ -6,12 +6,6 @@ The basic process goes from generating spectrograms using "SoX" into png files. 
 Every chunk is then fed to the CNN which extracts salient features to be passed to fully connected layers in order to perform categorical prediction. 
 
 
-![alt tag](https://raw.githubusercontent.com/Rhuax/MuGeRe/master/readme_images/000424_0.png?token=AIqauXQAeYm2ZiBAs0fMjCfeifgCDsVHks5aA1VPwA%3D%3D)
-![alt tag](https://raw.githubusercontent.com/Rhuax/MuGeRe/master/readme_images/000615_0.png?token=AIqaufiQL6qoe1NNP3ri6Fnl7jT_rUhIks5aA1VswA%3D%3D)
-![alt tag](https://raw.githubusercontent.com/Rhuax/MuGeRe/master/readme_images/000897_3.png?token=AIqauW2mN3x0kIHee-PQ1KBQTcJZYiXaks5aA1WMwA%3D%3D)
-![alt tag](https://raw.githubusercontent.com/Rhuax/MuGeRe/master/readme_images/006674_3.png?token=AIqauVPYVQhEi_OXxpOTmXnQx-n-SJ0Bks5aA1WhwA%3D%3D)
-![alt tag](https://raw.githubusercontent.com/Rhuax/MuGeRe/master/readme_images/024512_4.png?token=AIqauV4k-GEnzCA4GhApXSYiJ8z9yJfzks5aA1WvwA%3D%3D)
-
 # Requirements
 
 * Tensorflow
--- training.py
+++ training.py
@@ -11,13 +11,13 @@ from keras.layers import Dense, Dropout, Activation, Flatten, ZeroPadding2D
 from keras.layers import Conv2D, MaxPooling2D, AveragePooling2D
 from keras.callbacks import TensorBoard,ModelCheckpoint
 
-
-
-
-
-batchSize = 128
+batchSize = 32
 epochs = 30
-num_classes = 8
+num_classes = 16
+nb_train_examples=99924
+nb_valid_examples=25001
+
+
 tb=TensorBoard(batch_size=batchSize,log_dir='./logs')#logs
 model_path=datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H-%M-%S')
 os.mkdir('tuning_logs/'+model_path)
@@ -25,8 +25,8 @@ checkpoint = ModelCheckpoint('tuning_logs/'+model_path+'/'+model_path+'.hdf5', m
 
 
 callbacks=[tb,checkpoint]
-trainsetDir = 'fma_train/'
-testsetDir = 'fma_test/'
+trainsetDir = 'fma_medium_train/'
+testsetDir = 'fma_medium_test/'
 
 # Data generators
 train_datagen = ImageDataGenerator(rescale=1./255)
@@ -37,22 +37,20 @@ test_generator = test_datagen.flow_from_directory(directory=testsetDir, batch_si
 
 model = Sequential()
 
-model.add(Conv2D(input_shape=(160, 150, 3), filters=64, kernel_size=2, strides=2, activation="elu", kernel_initializer='glorot_normal'))
-model.add(MaxPooling2D(pool_size=2,padding='same'))
-
-model.add(Conv2D(filters=128, kernel_size=2, strides=2, activation='elu', kernel_initializer='glorot_normal'))
+model.add(Conv2D(input_shape=(160, 150, 3), filters=64, kernel_size=2, activation="elu", kernel_initializer='he_normal'))
+model.add(Conv2D(filters=128, kernel_size=2,strides=2,  activation='elu', kernel_initializer='he_normal'))
 model.add(MaxPooling2D(pool_size=2, padding='same'))
 
-model.add(Conv2D(filters=256, kernel_size=2, strides=2, activation='elu', kernel_initializer='glorot_normal'))
+model.add(Conv2D(filters=256, kernel_size=1,strides=1, activation='elu', kernel_initializer='he_normal'))
 model.add(MaxPooling2D(pool_size=2, padding='same'))
 
-model.add(Conv2D(filters=512, kernel_size=2, strides=2, activation='elu', kernel_initializer='glorot_normal'))
+model.add(Conv2D(filters=128, kernel_size=2,strides=2, activation='elu', kernel_initializer='he_normal'))
 model.add(MaxPooling2D(pool_size=2, padding='same'))
 
 model.add(Flatten())
-model.add(Dense(128))
+model.add(Dense(256))
 
-model.add(Activation('elu'))
+model.add(Activation('relu'))
 model.add(Dropout(0.5))
 
 model.add(Dense(num_classes, activation='softmax'))
@@ -63,13 +61,14 @@ model.compile(loss='categorical_crossentropy',
               metrics=['accuracy'])
 
 #Summary
+
+model.summary()
 old_stdoud = sys.stdout
 f=open('tuning_logs/'+model_path+'/'+model_path+'.txt','w')
 sys.stdout = f
 model.summary()
 f.close()
 sys.stdout = old_stdoud
-model.summary()
 f=open('tuning_logs/'+model_path+'/'+model_path+'_ARCH.json','w')
 f.write(model.to_json())
 f.close()
@@ -81,5 +80,5 @@ model.fit_generator(
         epochs=epochs,
         validation_data=test_generator,
         validation_steps=8000//batchSize,
-        steps_per_epoch=31985//batchSize,
+        steps_per_epoch=nb_train_examples//batchSize,
         callbacks=callbacks)
