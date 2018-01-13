ddf578b3be04f798f86ff6c0e1d8c12964c9c45d
Rhuax <gelidcrow@gmail.com>
2018-01-13 16:00:17 +0100

    Uploading best ResNet

--- test_set_eval.py
+++ test_set_eval.py
@@ -2,8 +2,7 @@ import numpy as np
 from keras.optimizers import SGD
 from keras.models import model_from_yaml,model_from_json
 from keras.preprocessing.image import ImageDataGenerator
-import os
-np.set_printoptions(suppress=True)
+np.set_printoptions(suppress=True,linewidth=300)
 
 img_width=160
 img_height=150
@@ -33,29 +32,54 @@ generator = datagen.flow_from_directory(
     print(i)"""
 
 predictions=model.predict_generator(generator,steps=482)
-
+confusion_matrix=np.zeros((10,10))
 accuracy=0
 for i,n in enumerate(generator.filenames):
-    if n.startswith("Classical") and np.argmax(predictions[i]) == 0:
-        accuracy += 1
-    elif n.startswith("Electronic") and np.argmax(predictions[i]) == 1:
-        accuracy += 1
-    elif n.startswith("Experimental") and np.argmax(predictions[i]) == 2:
-        accuracy += 1
-    elif n.startswith("Folk") and np.argmax(predictions[i]) == 3:
-        accuracy += 1
-    elif n.startswith("Hip-Hop") and np.argmax(predictions[i]) == 4:
-        accuracy += 1
-    elif n.startswith("Instrumental") and np.argmax(predictions[i]) == 5:
-        accuracy += 1
-    elif n.startswith("International") and np.argmax(predictions[i]) == 6:
-        accuracy += 1
-    elif n.startswith("Old-Time_Historic") and np.argmax(predictions[i]) == 7:
-        accuracy += 1
-    elif n.startswith("Pop") and np.argmax(predictions[i]) == 8:
-        accuracy += 1
-    elif n.startswith("Rock") and np.argmax(predictions[i]) == 9:
-        accuracy += 1
+    my_pred=np.argmax(predictions[i])
+    column=-1
+    if n.startswith("Classical"):
+        column = 0
+        if my_pred == 0:
+            accuracy += 1
+    elif n.startswith("Electronic"):
+        column = 1
+        if my_pred == 1:
+            accuracy += 1
+    elif n.startswith("Experimental"):
+        column = 2
+        if my_pred == 2:
+            accuracy += 1
+    elif n.startswith("Folk"):
+        column = 3
+        if my_pred == 3:
+            accuracy += 1
+    elif n.startswith("Hip-Hop"):
+        column = 4
+        if my_pred == 4:
+            accuracy += 1
+    elif n.startswith("Instrumental"):
+        column = 5
+        if my_pred == 5:
+            accuracy += 1
+    elif n.startswith("International"):
+        column = 6
+        if my_pred == 6:
+            accuracy += 1
+    elif n.startswith("Old-Time_Historic"):
+        column = 7
+        if my_pred == 7:
+            accuracy += 1
+    elif n.startswith("Pop"):
+        column = 8
+        if my_pred == 8:
+            accuracy += 1
+    elif n.startswith("Rock"):
+        column = 9
+        if my_pred == 9:
+            accuracy += 1
+    confusion_matrix[my_pred][column]+=1
+
+print(confusion_matrix)
 print(accuracy/(np.shape(predictions)[0]))
 
 
