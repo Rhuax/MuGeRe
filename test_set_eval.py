[FIXED] Accuracy calculation

test_set_eval.py
@@ -10,27 +10,27 @@ img_height = 150
 
 def compare_song_class(prediction, genre):
     if genre.startswith("Classical") and prediction == 0:
-        return prediction
+        return True
     elif genre.startswith("Electronic") and prediction == 1:
-        return prediction
+        return True
     elif genre.startswith("Experimental") and prediction == 2:
-        return prediction
+        return True
     elif genre.startswith("Folk") and prediction == 3:
-        return prediction
+        return True
     elif genre.startswith("Hip-Hop") and prediction == 4:
-        return prediction
+        return True
     elif genre.startswith("Instrumental") and prediction == 5:
-        return prediction
+        return True
     elif genre.startswith("International") and prediction == 6:
-        return prediction
+        return True
     elif genre.startswith("Old-Time_Historic") and prediction == 7:
-        return prediction
+        return True
     elif genre.startswith("Pop") and prediction == 8:
-        return prediction
+        return True
     elif genre.startswith("Rock") and prediction == 9:
-        return prediction
+        return True
     else:
-        return -1
+        return False
 
 f = open('tuning_logs/2018-01-12 02-33-45/2018-01-12 02-33-45_ARCH.json', 'r')
 model = model_from_json(f.read())
@@ -52,7 +52,9 @@ generator = datagen.flow_from_directory(
         shuffle=False)
 
 # Initialization
-confusion_matrix = np.zeros((10, 10))
+#confusion_matrix = np.zeros((10, 10))
+right = np.zeros(10)
+wrong = np.zeros(10)
 accuracy = 0
 song_genres = np.zeros(10)
 spectrogram = -1
@@ -62,21 +64,23 @@ predictions = model.predict_generator(generator, steps=482)
 for i, n in enumerate(sorted(generator.filenames)):
     my_pred = np.argmax(predictions[i])
     spectrogram += 1
-    if spectrogram == 5:
+    if spectrogram == 4:
         # If prediction is valid, update accuracy
-        column = compare_song_class(np.argmax(song_genres), n)      # Controllare
-        if column != -1:
+        if compare_song_class(np.argmax(song_genres), n):
             accuracy += 1
+            right[my_pred] += 1
+        else:
+            wrong[my_pred] += 1
         # Reset all variables
         spectrogram = -1
         song_genres = np.zeros(10)
-        # Update confusion matrix
-        # La matrice di confusione è andata a puttane perché quando non azzecca perdo il valore di column che è a -1, kek sry
-        confusion_matrix[my_pred][column] += 1
-    else:
-        song_genres[my_pred] += predictions[i][my_pred]             # Controllare
+    song_genres[my_pred] += predictions[i][my_pred]             # Controllare
 
-print(confusion_matrix)
+print("Correctly classified songs: ")
+print(right)
+print("Misclassified songs: ")
+print(wrong)
+print("Total accuracy on test set: ")
 print(accuracy/((np.shape(predictions)[0])//5))
 
 