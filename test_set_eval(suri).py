24eb880efd4f89c77f163c7ee6fccc8fe686c790
Francesco Suriano <fra.suri94@gmail.com>
2018-01-19 11:53:27 +0100

    New tries, F3 best

---
+++ test_set_eval(suri).py
@@ -0,0 +1,130 @@
+import numpy as np
+from keras.optimizers import SGD, rmsprop
+from keras.models import model_from_json, model_from_yaml
+from keras.preprocessing.image import ImageDataGenerator
+np.set_printoptions(suppress=True,linewidth=300)
+
+img_width = 160
+img_height = 150
+
+
+def compare_song_class(prediction, genre):
+    if genre.startswith("Classical") and prediction == 0:
+        return True
+    elif genre.startswith("Electronic") and prediction == 1:
+        return True
+    elif genre.startswith("Experimental") and prediction == 2:
+        return True
+    elif genre.startswith("Folk") and prediction == 3:
+        return True
+    elif genre.startswith("Hip-Hop") and prediction == 4:
+        return True
+    elif genre.startswith("Instrumental") and prediction == 5:
+        return True
+    elif genre.startswith("International") and prediction == 6:
+        return True
+    elif genre.startswith("Old-Time_Historic") and prediction == 7:
+        return True
+    elif genre.startswith("Pop") and prediction == 8:
+        return True
+    elif genre.startswith("Rock") and prediction == 9:
+        return True
+    else:
+        return False
+
+
+def def_genre_from_str(genre):
+    if genre.startswith("Classical"):
+        return 0
+    elif genre.startswith("Electronic"):
+        return 1
+    elif genre.startswith("Experimental"):
+        return 2
+    elif genre.startswith("Folk"):
+        return 3
+    elif genre.startswith("Hip-Hop"):
+        return 4
+    elif genre.startswith("Instrumental"):
+        return 5
+    elif genre.startswith("International"):
+        return 6
+    elif genre.startswith("Old-Time_Historic"):
+        return 7
+    elif genre.startswith("Pop"):
+        return 8
+    elif genre.startswith("Rock"):
+        return 9
+
+f = open('arch_weights/MuGeRe Architecture_F3.yaml', 'r')
+model = model_from_yaml(f.read())
+f.close()
+model.load_weights('arch_weights/MuGeRe Weights_F3.h5')
+
+model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+
+model.summary()
+datagen = ImageDataGenerator(
+        rescale=1. / 255
+    )
+
+generator = datagen.flow_from_directory(
+        'fma_medium_test/',
+        target_size=(img_width, img_height),
+        batch_size=50,
+        class_mode=None,
+        shuffle=False)
+
+# Initialization
+confusion_matrix = np.zeros((10, 10))
+right = np.zeros(10)
+wrong = np.zeros(10)
+accuracy = 0
+song_genres = np.zeros(10)
+spectrogram = -1
+
+predictions = model.predict_generator(generator, steps=482)
+for i, n in enumerate(sorted(generator.filenames)):
+    my_pred = np.argmax(predictions[i])
+    spectrogram += 1
+    if spectrogram == 4:
+        # If prediction is valid, update accuracy
+        if compare_song_class(np.argmax(song_genres), n):
+            accuracy += 1
+            right[my_pred] += 1
+        else:
+            wrong[my_pred] += 1
+        # Reset all variables
+        spectrogram = -1
+        song_genres = np.zeros(10)
+    confusion_matrix[my_pred][def_genre_from_str(n)] += 1
+    song_genres += predictions[i]
+
+
+for i in range(10):
+    if i == 0:
+        print("Classica")
+    elif i == 1:
+        print("Electronic")
+    elif i == 2:
+        print("Experimental")
+    elif i == 3:
+        print("Folk")
+    elif i == 4:
+        print("Hip-Hop")
+    elif i == 5:
+        print("Instrumental")
+    elif i == 6:
+        print("International")
+    elif i == 7:
+        print("Old Time Historic")
+    elif i == 8:
+        print("Pop")
+    elif i == 9:
+        print("Rock")
+    print('{} su {} - Percentuale {}'.format(right[i], right[i] + wrong[i], right[i] / (right[i] + wrong[i])))
+print("Total accuracy on test set: ")
+print(accuracy/((np.shape(predictions)[0])//5))
+print("\nConfusion matrix:")
+print(confusion_matrix)
+
+
--- training_2.py
+++ training_2.py
@@ -5,7 +5,7 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten, ZeroPadding2D, LeakyReLU, Reshape
 from keras.layers import Conv2D, MaxPooling2D, AveragePooling2D
-from keras.optimizers import Adadelta
+from keras.optimizers import Adadelta, sgd
 
 
 
@@ -21,9 +21,9 @@ import os
 
 batchSize = 128
 
-epochs = 30
+epochs = 12
 
-num_classes = 16
+num_classes = 10
 
 model_name = 'Genre Recognition Model'
 
@@ -74,22 +74,22 @@ model.add(Dropout(0.5))
 model.add(Dense(num_classes, activation='softmax'))
 '''
 
-
 model = Sequential()
 
-model.add(Conv2D(input_shape=(160, 150, 3), filters=32, kernel_size=(5,5), strides=(5,5), activation="elu", kernel_initializer='glorot_normal'))
-model.add(Conv2D(filters=64, kernel_size=(3,3), strides=(3,3),activation="elu", kernel_initializer='glorot_normal'))
-model.add(Conv2D(filters=128, kernel_size=(2,2), strides=(2,2),activation="elu", kernel_initializer='glorot_normal'))
-model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
+model.add(Conv2D(input_shape=(160, 150, 3), filters=32, kernel_size=(2,15), strides=(2,15), activation="elu", kernel_initializer='glorot_normal'))
+model.add(Conv2D(filters=16, kernel_size=(3,3), strides=(3,3),activation="elu", kernel_initializer='glorot_normal'))
+#model.add(MaxPooling2D(pool_size=(2, 2), strides=2))
+model.add(Conv2D(filters=8, kernel_size=(2,2), strides=(2,2), activation="elu", kernel_initializer='glorot_normal'))
 model.add(Flatten())
-#model.add(Dense(512, activation='elu', kernel_initializer='glorot_normal'))
+model.add(Dropout(0.5))
+model.add(Dense(128, activation='elu', kernel_initializer='glorot_normal'))
 model.add(Dense(num_classes, activation='softmax'))
 
 
 #Compile model
 
 optimizer = 'adam'
-
+#optimizer = sgd(lr=0.01, momentum=0.8, decay=0.0, nesterov=True)
 model.compile(loss='categorical_crossentropy',
 
               optimizer=optimizer,
@@ -105,7 +105,7 @@ model.summary()
 
 
 #Training
-#model.load_weights('MuGeRe Weights_T4.h5', by_name=True)
+#model.load_weights('arch_weights/MuGeRe Weights_F1.h5', by_name=True)
 model.fit_generator(
 
         train_generator,
@@ -114,15 +114,15 @@ model.fit_generator(
 
         validation_data=test_generator,
 
-        validation_steps=25001//batchSize,
+        validation_steps=24066//batchSize,
 
-        steps_per_epoch=99924//batchSize)
+        steps_per_epoch=96214//batchSize)
 
 
 # serialize model to YAML
 model_yaml = model.to_yaml()
-with open("MuGeRe Architecture.yaml", "w") as yaml_file:
+with open("MuGeRe Architecture_F.yaml", "w") as yaml_file:
     yaml_file.write(model_yaml)
 # serialize weights to HDF5
-model.save_weights("MuGeRe Weights.h5")
+model.save_weights("MuGeRe Weights_F.h5")
 print("Saved model to disk")
